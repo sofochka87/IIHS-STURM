34d5b83bd9e9eb05f7087be900f510e9b50cd2d3
skyldrdm <email@email.com>
2018-08-06 17:15:27 -0400

    xml

--- src/test/java/com/iihs/tests/MozBase.java
+++ src/test/java/com/iihs/tests/MozBase.java
@@ -8,11 +8,11 @@ import com.iihs.pages.Fatality_Facts;
 import com.iihs.pages.SPA_973_MOzPages;
 import com.iihs.pages.SPA_974_MOzPages;
 import com.iihs.pages.SPA_975_MOzPages;
-import com.iihs.pages.SPA_976_MOzPages;
 
 public class MozBase extends TestBase {
 	
-	protected Fatality_Facts facts = new Fatality_Facts();
+	//protected Fatality_Facts facts = new Fatality_Facts();
+	
 	protected SPA_973_MOzPages webElems973 = new SPA_973_MOzPages();
 	protected SPA_974_MOzPages webElems974 = new SPA_974_MOzPages();
 	protected SPA_975_MOzPages webElems975 = new SPA_975_MOzPages();
